913da1d4f04f8a4143586ddd3eb2d3af24252919
pitassarii53-lab <pitassarii53@gmail.com>
2026-02-05 09:11:51 +0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -12,10 +12,26 @@ export default function RootLayout({ children }) {
   return (
     <html lang="id">
       <head>
+        {/* IKLAN ADSTERRA */}
         <Script 
           src="https://pl27333728.effectivegatecpm.com/e4/6a/bf/e46abf385099c2b5d894dbb1c522e30c.js" 
           strategy="afterInteractive" 
         />
+
+        {/* SCRIPT HISTATS (Ganti 4900000 dengan ID Histats kamu) */}
+        <Script id="histats-counter" strategy="afterInteractive">
+          {`
+            var _Hasync= _Hasync|| [];
+            _Hasync.push(['Histats.start', '1,7301723,4,0,0,0,00010000']);
+            _Hasync.push(['Histats.fasi', '1']);
+            _Hasync.push(['Histats.track_hits', '']);
+            (function() {
+            var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
+            hs.src = ('//s10.histats.com/js15_as.js');
+            (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
+            })();
+          `}
+        </Script>
       </head>
       <body style={{ margin: 0, backgroundColor: '#000', color: '#fff', minHeight: '100vh' }}>
         {children}
@@ -54,6 +70,13 @@ export default function RootLayout({ children }) {
             }}></span>
           </a>
         </div>
+
+        {/* BACKUP HISTATS (Ganti 4900000 dengan ID Histats kamu) */}
+        <noscript>
+          <a href="/" target="_blank">
+            <img src="//sstatic1.histats.com/0.gif?7301723&101" alt="histats" border="0" />
+          </a>
+        </noscript>
       </body>
     </html>
   );
